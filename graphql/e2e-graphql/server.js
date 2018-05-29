f42a805d0f82f459ccbdabc176abde44419c4f66
harrythedev <harrythedev@gmail.com>
2018-05-29 14:07:33 -0400

    add server

---
+++ graphql/e2e-graphql/server.js
@@ -0,0 +1,45 @@
+mport express from 'express'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import dotenv from 'dotenv'
+import { ApolloEngine } from 'apollo-engine'
+
+import schema from './data/schema'
+
+dotenv.config()
+const { GRAPHQL_PORT, ENGINE_API_KEY } = process.env // find key from https://engine.apollographql.com
+
+const app = express()
+
+app.use(compression()) // gzip compression for HTTP response
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    tracing: true,
+    cacheControl: true,
+  })
+)
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+const engine = new ApolloEngine({
+  apiKey: ENGINE_API_KEY,
+  stores: [
+    {
+      name: 'inMemEmbeddedCache',
+      inMemory: {
+        cacheSize: 104857600, // 100 MB; defaults to 50MB.
+      },
+    },
+  ],
+  queryCache: {
+    publicFullQueryStore: 'inMemEmbeddedCache',
+  },
+})
+
+engine.listen({
+  port: GRAPHQL_PORT,
+  expressApp: app,
+})
