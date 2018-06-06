482150b0dbb6ed4fca548ae6a3462eae1b16e33f
harrythedev <harrythedev@gmail.com>
2018-06-06 15:53:59 -0400

    basic setup

--- graphql/hackernews-node/src/index.js
+++ graphql/hackernews-node/src/index.js
@@ -1,20 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
+// import { GraphQLServer } from 'graphql-yoga'
 
+// defines GraphQL schema: Query{ info: String! }
 const typeDefs = `
 type Query {
   info: String!
 }
 `
 
+// implementation of schema: typeDefs: Query.info
 const resolvers = {
   Query: {
-    info: () => `This is the API of Hackernews clone`
+    info: () => `This is the API of a Hackernews Clone`,
   }
 }
 
+// bundled and passed to GraphQLServer.
+// tells server what API operations are accepted and how they should be resolved.
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+// visit http://localhost:4000 to use Playground
+server.start(() => console.log(`Server is runing on http://localhost:4000`))
